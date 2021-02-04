fbc7281c49cea5740442bbb6c5ad44630b3eabe9
dineshk033 <dineshk033@gmail.com>
2021-02-04 17:00:58 +0530

    Updates

---
+++ precache-manifest.3d504352148f97c8951e485e5abb9282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2a195707180dc2a05319401e417085",
+    "url": "/kpatracker/index.html"
+  },
+  {
+    "revision": "7a92869bc57793110b5a",
+    "url": "/kpatracker/static/css/main.b0b15e72.chunk.css"
+  },
+  {
+    "revision": "c5c3a59242a895e2b82c",
+    "url": "/kpatracker/static/js/2.c636d0db.chunk.js"
+  },
+  {
+    "revision": "d57e74171e5ce0c9384f984a9123bbd6",
+    "url": "/kpatracker/static/js/2.c636d0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a92869bc57793110b5a",
+    "url": "/kpatracker/static/js/main.b71e270c.chunk.js"
+  },
+  {
+    "revision": "fb0e45751663085c86b6",
+    "url": "/kpatracker/static/js/runtime-main.b4a266b7.js"
+  }
+]);
